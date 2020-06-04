64d819d62ddee8e1b454b238c0703cc885625267
Evan Coleman <e@edc.me>
2020-06-04 13:58:00 -0400

    update formular

--- formula.rb
+++ formula.rb
@@ -1,9 +1,9 @@
 class Bluepill < Formula
   desc "Testing tool for iOS that runs UI tests using multiple simulators"
   homepage "https://github.com/tkww/bluepill"
-  url "https://github.com/tkww/bluepill.git",
-    :revision => "f1bc12d3292501529a9d9e41d86d7e381ffbe025"
-  head "https://github.com/tkww/bluepill.git"
+  url "https://github.com/tkww/bluepill/releases/download/v5.2.2-1/Bluepill.zip"
+  sha256 "c97fc0fb60387dc416a76b323ee28ee42a4a0e0fc69ae01458978a5d13b81929"
+  head "https://github.com/tkww/bluepill.git", :branch => "master"
 
   depends_on :xcode => ["11.2", :build]
 
